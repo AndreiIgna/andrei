84220084047248e2f4fb69bc42a8fa754604a18b
Andrei <andrei@laye.red>
2022-07-23 13:13:10 +0200

    Updated - moved app css to separate file

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,9 @@
+<script>
+import '../app.css';
 
-<style lang="css">
-.site {
-	padding: 0;
-	margin: 0;
-	font: 400 16px / 1.5em -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-}
 
-h1, h2, h3, h4, h5, h6 {
-	font: 700 48px / 1.05em Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-	letter-spacing: -0.01em;
-	color: rgb(255, 222, 211);
 }
-</style>
+</script>
 
 <div class="site">
 	<slot></slot>
